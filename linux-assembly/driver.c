c48274bd363ece2ec95cc649fc21a25bb4a65389
albert <learning.albert.zhang@gmail.com>
2018-12-18 16:23:27 +0800

    [asm] Add raise authority code in driver.c using Linux C
    Signed-off-by: albert <learning.albert.zhang@gmail.com>

--- linux-assembly/driver.c
+++ linux-assembly/driver.c
@@ -1,10 +1,16 @@
 #include "cdecl.h"
+#include <sys/io.h>
+#include <stdio.h>
 
 int PRE_CDECL asm_main( void ) POST_CDECL;
 
 int main()
 {
     int ret_status;
+    if (iopl(3) < 0) {
+        printf("raise privilege failed!\n");
+        return 1;
+    }
     ret_status = asm_main();
     return ret_status;
 }
